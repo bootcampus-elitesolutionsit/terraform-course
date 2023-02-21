a1f068245152f81baa1c982f237a9fafcc14343b
ADENIKE2010 <adenikeodusanya@outlook.com>
2023-02-20 22:21:48 -0600

    moved from my branch

---
+++ myec2.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "project_server" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  key_name               = aws_key_pair.project_key.key_name
+  tags = {
+    Name = "projectserver"
+    Env  = "dev"
+  }
+}
+
+resource "aws_key_pair" "project_key" {
+  key_name   = var.key_name
+  public_key = var.public_key
+}
